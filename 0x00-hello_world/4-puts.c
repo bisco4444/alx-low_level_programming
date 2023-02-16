9bf9ccd6532e582ce5cf80c136bdb8bad4d01e71
Bismark <bismarkowiredu@gmail.com>
2023-02-16 13:40:38 -0800

    Task 0-6. C basics

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#iclude < stdio .h >
+/**
+ * main - A program that prints a line with puts function
+ * return: 0 (successful)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include < stdio.h >
+/**
+ * main -  prints the size of various types on the computer it is compiled
+ * Return 0(successful)
+ */
+int main(void)
+{
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float f;
+printf("size of char: %lu byte(s)\n", (unsigned long)size of(a));
+printf("size of int: %lu byte(s)\n", (unsigned long)size of(b));
+printf("size of long: %lu byte(s)\n", (unsigned long)size of(c));
+printf("size of long long int: %lu byte(s)\n", (unsigned long)size of(d));
+printf("size of float: %lu byte(s)\n", (unsigned long)size of(f));
+return (0);
+}
