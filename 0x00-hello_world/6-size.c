bd1611124cab04fb985f669ef4564e6bceccc5d2
Bismark <bismarkowiredu@gmail.com>
2023-02-16 16:39:22 -0800

    fix

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,11 +10,11 @@ int main(void)
 	long int c;
 	long long int d;
 	float f;
-printf("size of char: %lu byte(s)\n", (unsigned long)size of(a));
-printf("size of int: %lu byte(s)\n", (unsigned long)size of(b));
-printf("size of long: %lu byte(s)\n", (unsigned long)size of(c));
-printf("size of long long int: %lu byte(s)\n", (unsigned long)size of(d));
-printf("size of float: %lu byte(s)\n", (unsigned long)size of(f));
+printf("size of char: %lu byte(s)\n", (unsigned long)sizeof(a));
+printf("size of int: %lu byte(s)\n", (unsigned long)sizeof(b));
+printf("size of long: %lu byte(s)\n", (unsigned long)sizeof(c));
+printf("size of long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
+printf("size of float: %lu byte(s)\n", (unsigned long)sizeof(f));
 return (0);
 }
 
