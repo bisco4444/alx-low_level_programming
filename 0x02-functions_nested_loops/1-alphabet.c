8ce7a8dbfaeec8fb97c152802342df6108973f1d
Bismark <bismarkowiredu@gmail.com>
2023-02-22 17:59:27 -0800

    Alpha1 Alpha2

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * Main - prints alpha in lowercase
+ */
+void print_alphabet(void)
+{
+	char alpha;
+
+	for (alpha = 'a'; alpha <= z; alpha++)
+	_putchar(alpha);
+	_putchar('\n');
+return (0);
+}
---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * Main - prints alpha in lowercase
+ */
+void print_alphabet(void)
+{
+	char alpha;
+	int t;
+
+	for (t = 0; t <= 9; t++)
+	{
+		for (alpha = 'a'; alpha <= 'z' alpha++)
+			_putchar(alpha);
+		putchar('\n');
+	}
+}
