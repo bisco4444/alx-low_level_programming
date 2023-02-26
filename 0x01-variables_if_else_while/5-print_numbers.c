6e94fd96e5ff0b7cdde259f5257a1e393ef31cda
Bismark <bismarkowiredu@gmail.com>
2023-02-25 16:41:04 -0800

    Task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/***
+ * main - Entry
+ * Description: print single digit in base10
+ * Return: Always (0)
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+		printf("%d", a);
+	printf("\n");
+	return (0);
+}
